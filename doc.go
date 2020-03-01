c93334fa4999b733d58d4c34a2c341e1ee1c1444
billsjc <sjc@debug.online>
2020-03-01 20:24:06 +0800

    update doc sample

--- doc.go
+++ doc.go
@@ -12,8 +12,8 @@
 // 		resp,err := pageable.PageQuery(page,rowsPerPage,handler,&resultSet)
 // 		// handle error
 // 		f err != nil {
-// 		panic(err)
-// 		}
+// 			panic(err)
+// 	}
 // And then you can print this value to see the page info
 // 		fmt.Println(resp.PageNow)    //PageNow: current page of query
 // 		fmt.Println(resp.PageCount)  //PageCount: total page of the query
@@ -23,5 +23,16 @@
 // 		fmt.Println(resp.FirstPage)  //FirstPage: if the result is the first page
 // 		fmt.Println(resp.LastPage)   //LastPage: if the result is the last page
 // 		fmt.Println(resp.Empty)  //Empty: if the result is empty
-// 		}
+// 	}
+// And here a clear JSON object of the Response LIKE Spring Pageable
+// 	{
+// 		"PageNow": 2,
+// 		"PageCount": 1,
+// 		"RawCount": 1,
+// 		"RawPerPage": 25,
+// 		"ResultSet": [],
+// 		"FirstPage": false,
+// 		"LastPage": false,
+// 		"Empty": true
+// 	}
 package pageable
